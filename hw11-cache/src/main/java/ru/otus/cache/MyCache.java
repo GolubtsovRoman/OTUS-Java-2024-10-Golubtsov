80c3d6129fac4f20357f3c89cbbac44d699d4a8b
Roman Golubtsov <roman.golubtsov@taskdata.ru>
2025-02-01 12:14:54 +0300

    ДЗ№11 Свой cache engine
    Подготовка файлов

---
+++ hw11-cache/src/main/java/ru/otus/cache/MyCache.java
@@ -0,0 +1,32 @@
+package ru.otus.cache;
+
+import ru.otus.cache.listener.HwListener;
+
+public class MyCache<K, V> implements HwCache<K, V> {
+    // Надо реализовать эти методы
+
+    @Override
+    public void put(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addListener(HwListener<K, V> listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeListener(HwListener<K, V> listener) {
+        throw new UnsupportedOperationException();
+    }
+}
