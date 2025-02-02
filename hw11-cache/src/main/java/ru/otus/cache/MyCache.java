5c538a8e7ce82cb351423d565af305aa81f2aecb
Roman Golubtsov <roman.golubtsov@taskdata.ru>
2025-02-02 11:05:50 +0300

    ДЗ№11 Свой cache engine
    Реализация кэша

--- hw11-cache/src/main/java/ru/otus/cache/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cache/MyCache.java
@@ -2,31 +2,50 @@ package ru.otus.cache;
 
 import ru.otus.cache.listener.HwListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
+
+    Map<K, V> cache = new WeakHashMap<>();
+
+    List<HwListener<K, V>> listeners = new ArrayList<>();
+
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        runListeners(key, value, "added");
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        var value = cache.remove(key);
+        runListeners(key, value, "removed");
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        var value = cache.get(key);
+        runListeners(key, value, "got");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
     }
+
+
+    private void runListeners(K key, V value, String action) {
+        listeners.forEach(listener -> listener.notify(key, value, action));
+    }
+
 }
